ca0d251edbb8282addbfb1c6c93f5559f5a021f0
robertoface <reynolds.37@gmail.com>
2021-05-11 21:06:37 +0100

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express'); // searches node_modules for "express"
 const app = express(); // is an instance of the above constructor
 
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
+
+
 app.listen(8000,function(){
-    console.log("server is running")
+    console.log("server is running")  // listening on port 8000, would this change per live hosting???
     });

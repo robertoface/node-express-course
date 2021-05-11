b850095f93da1930efe09d063d00c9c292b8d719
robertoface <reynolds.37@gmail.com>
2021-05-11 21:28:12 +0100

    add login POST route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
     ]
+    
     app.get('/users', function(req,res){
          res.json({
               success: true,
@@ -14,7 +15,7 @@ const mockUserData=[
          })
     })
 
-    app.get('/users/:id',function(req,res){
+    app.get('/users/:id',function(req,res){  //basically a GET request on the back-end
         console.log(req.params.id)
         res.json({
             success: true,
@@ -22,7 +23,33 @@ const mockUserData=[
             user: req.params.id
         })
     })
+
+
+    const bodyParser = require('body-parser'); // is a POST request on the back-end 
+    app.use(bodyParser.json());
+  
+    app.post('/login',function(req,res){
+        const username=req.body.username;
+        const password=req.body.password;
     
-app.listen(8000,function(){
-    console.log("server is running")  // listening on port 8000, would this change per live hosting???
+        const mockUsername="billyTheKid";
+        const mockPassword="superSecret";
+    
+        if (username===mockUsername && password===mockPassword){  // if-else statement to see that username and password both match
+             res.json({
+                  success: true,
+                  message: 'password and username match!',
+                  token: 'encrypted token goes here'
+             })
+        } else {
+             res.json({
+                  success: false,
+                  message: 'password and username do not match'
+             })
+        }
+   })
+
+
+    app.listen(8000,function(){
+        console.log("server is running")  // listening on port 8000, would this change per live hosting???
     });
